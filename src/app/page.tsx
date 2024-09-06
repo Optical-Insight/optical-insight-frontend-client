7a50285ffb95dfe7d8bfc38cc1772f9c83cd2826
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-09-06 21:06:03 +0530

    deploying to versal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,6 +67,23 @@ export default function Home() {
           All Right Researved
         </a>
       </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Kithmina Siriwardana
+        </a>
+      </footer>
     </div>
   );
 }

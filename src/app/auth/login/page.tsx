26d60f51f9d1f208ccb574febe5979cf42ba92e6
kithmina-siriwardana <it21020230@my.sliit.lk>
2024-09-08 23:57:06 +0530

    trying login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -10,44 +10,59 @@ function PatientLogin() {
     console.log("Login clicked");
   };
 
-  console.log("setLoginLabel", setLoginLabel);
+  console.log("loginLabel", setLoginLabel);
 
   // const handleChangeLoginMethod = () => {
   //   setLoginLabel(loginLabel === "Email" ? "Phone" : "Email");
   // };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center bg-lightBg">
-      <div className="flex flex-col items-center">
-        <Image
-          src={"/assets/images/logo.png"}
-          alt={"logo"}
-          height={80}
-          width={200}
-        />
-      </div>
-
-      <div className="mt-[24%] mx-[6%]">
-        <p className="text-4xl text-headerText font-bold">Login</p>
-
-        <div className="mt-[12%]">
-          <label className="block text-md  text-labelText">
-            {loginLabel === "Email" ? "Email Address" : "Phone Number"}
-          </label>
-          <input
-            placeholder={
-              loginLabel === "Email"
-                ? "kithminasiriwardaan@gmail.com"
-                : "0123456789"
-            }
-            className="w-full rounded-lg text-sm h-10 mt-[1.5%]"
+    <div className="w-screen h-screen flex flex-col justify-between bg-lightBg">
+      <div className="mt-[35%]">
+        {/* HEADER */}
+        <div className="flex flex-col items-center">
+          <Image
+            src={"/assets/images/logo.png"}
+            alt={"logo"}
+            height={80}
+            width={200}
           />
         </div>
 
-        <div className="w-full rounded-lg h-10 mt-[6%]">
-          <CommonBtn label="Send OTP" onClick={handleSubmitLogin} />
-          <p className="text-blueText">Or Continue With Phone</p>
+        {/* FORM */}
+        <div className="mt-[24%] mx-[6%]">
+          <p className="text-4xl text-headerText font-bold">Login</p>
+
+          <div className="mt-[12%]">
+            <label className="block text-md  text-labelText">
+              {loginLabel === "Email" ? "Email Address" : "Phone Number"}
+            </label>
+            <input
+              placeholder={
+                loginLabel === "Email"
+                  ? "kithminasiriwardaan@gmail.com"
+                  : "0123456789"
+              }
+              className="w-full rounded-lg text-md h-12 mt-[1.5%]"
+            />
+          </div>
+
+          <div className="w-full rounded-lg h-12 mt-[6%]">
+            <CommonBtn label="Send OTP" onClick={handleSubmitLogin} />
+          </div>
+
+          <div className="flex justify-center mt-[1%]">
+            <p className="text-blueText">Or Continue With Phone</p>
+          </div>
         </div>
+      </div>
+
+      {/* FOOTER */}
+      <div className="text-center text-sm text-blueText mx-[15%] mb-[15%]">
+        <p>
+          Visit your nearest channeling center to create an optical insight
+          account
+        </p>
       </div>
     </div>
   );

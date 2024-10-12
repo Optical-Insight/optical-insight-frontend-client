9b5d72487d723a87e3e39e3be350528375c187e4
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-10-12 17:28:41 +0530

    cookie issues fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,30 +2,19 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  // Get the authData (or token) from cookies
   const token = req.cookies.get("authData");
 
   // Define routes that require authentication
   const protectedRoutes = ["/dashboard"];
 
-  // const loginRoute = ["/auth/login"];
+  console.log("middleware");
 
-  // If the user tries to access a protected route but isn't authenticated, redirect them to login
   if (
     !token &&
     protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
   ) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
-
-  // If the user tries to access login route but authenticated, redirect them to dashboard
-  // if (
-  //   token &&
-  //   loginRoute.some((route) => req.nextUrl.pathname.startsWith(route))
-  // ) {
-  //   return NextResponse.redirect(new URL("/dashboard", req.url));
-  // }
-
   return NextResponse.next();
 }
 
